d95c5a1e2ab61ff1c16cfe80bbab6a78adc3403d
suryamohan1919 <suryamohan1919@gmail.com>
2022-03-14 16:58:44 +0530

    Add chromium dependency for puppeteer in testtunner

--- test-runner.sh
+++ test-runner.sh
@@ -7,6 +7,7 @@ testtype=$1
 if [[ $testtype = "e2e-tests" ]]
 then
     echo "Running e2e-tests using headless chrome"
+    apt-get -y install libgbm1
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
         && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
     apt-get update && apt-get -y install google-chrome-stable
